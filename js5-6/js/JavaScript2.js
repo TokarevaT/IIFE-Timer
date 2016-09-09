6338a47141ade1b2680f1b4ed1b28f8927844b56
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-09-10 02:42:05 +0300

    add homework with closure and prototype

---
+++ js5-6/js/JavaScript2.js
@@ -0,0 +1,50 @@
+﻿//ДЗ Prototype
+function Factory(init, animals) {
+    this.init = 'type';
+    this.animals = ['dog', 'cat'];
+};
+function Dog() {
+    this.say = alert('I am a dog!');
+    __proto__: Factory;
+};
+Dog();
+
+function Cat(){
+    this.say = alert('I am a cat!');
+    __proto__: Factory;
+};
+Cat();
+
+var newFactory = new Factory;
+var newDog = new Dog();
+var newCat = new Cat();
+console.log('newDog =', newDog);
+console.log('newCat =', newCat);
+
+
+//ДЗ № Closure 3
+function FunctionArray(count) {
+    var arr = [], i;
+    for (i = 0; i < count; i++) {
+        (function (index) {
+            arr.push(function () { alert(index) });
+        }(i));
+    }
+    return arr;
+}
+//OR
+function generateArr() {
+    var arr = [];
+    for (var i = 0; i < count; i++) {
+        arr[i] = makeFunction(i);
+    }
+    return arr;
+}
+
+function makeFunction(i) {
+    return function () {
+        alert(i);
+    }
+}
+
+
---
+++ js5-6/js/JavaScript3.js
@@ -0,0 +1,30 @@
+﻿////ДЗ Closure 2
+
+
+Function.prototype.bind = function (func, context) {
+    return function () { 
+                return func.apply(context, arguments);
+            };
+}
+
+
+var HelloPage = {
+    name: 'GoIT',
+    init: function () {
+        console.log('Hello, ' + this.name);
+    }
+}
+
+
+window.onload = HelloPage.init.bind(HelloPage.init, HelloPage);
+
+//Closure
+(function (count) {
+    if (count < 5) {
+        console.log(count);
+        var caller = arguments.callee;
+        window.setTimeout(function () {
+            caller(count + 1);
+        }, 1000);
+    }
+})(0);
